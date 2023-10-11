fb4476ae64c7df23f9c86a38e950d8b4cd93d37e
Average Catgirl <125671704+TabbyisTrans@users.noreply.github.com>
2023-10-11 13:12:28 -0400

    Add files via upload

---
+++ apcsa/newnumriddle/newriddle.java
@@ -0,0 +1,14 @@
+/*
+This is a program that proves that no matter the type of number imputed into this equation, the answer always equals three.
+*/
+public class newriddle {
+    public static void main(String[] args) 
+    {
+        numriddle.solveRiddle (5);
+        numriddle.solveRiddle (-7);
+        numriddle.solveRiddle (0);
+        numriddle.solveRiddle (1);
+        numriddle.solveRiddle (11.5);
+        numriddle.solveRiddle (-20.5);
+    }
+}
---
+++ apcsa/newnumriddle/numriddle.java
@@ -0,0 +1,32 @@
+public class numriddle
+{
+    public static void solveRiddle (double num) 
+    {
+        double innum = num;
+        System.out.print("Number you chose: " + num + "\n" ); // prints number you chose
+        num *= 2; // doubles number
+        System.out.println("your number doubled: " + num); // prints results
+        num += 6; // adds six
+        System.out.println("your number plus six: " + num); // prints results
+        num /= 2; // halves
+        System.out.println("your number halved: " + num); // prints results
+        num -= innum; // subracting original number
+        System.out.println("your final number is: " + num); // prints final number
+        System.out.println();
+    }
+
+    public static void solveRiddle (int num) 
+    {
+        int innum = num;
+        System.out.print("Number you chose: " + num + "\n" ); // prints number you chose
+        num *= 2; // doubles number
+        System.out.println("your number doubled: " + num); // prints results
+        num += 6; // adds six
+        System.out.println("your number plus six: " + num); // prints results
+        num /= 2; // halves
+        System.out.println("your number halved: " + num); // prints results
+        num -= innum; // subracting original number
+        System.out.println("your final number is: " + num); // prints final number
+        System.out.println();
+    }
+}
