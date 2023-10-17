fa35677c82ab22b316744d9a11e12b7fbd437dea
Average Catgirl <125671704+TabbyisTrans@users.noreply.github.com>
2023-10-17 17:15:05 +0000

    fixed spelling errors and formatting adjustments

--- apcsa/newnumriddle/numriddle.java
+++ apcsa/newnumriddle/numriddle.java
@@ -1,31 +1,28 @@
-public class numriddle
-{
-    public static void solveRiddle (double num) 
-    {
+public class numriddle {
+    public static void solveRiddle(double num) {
         double innum = num;
-        System.out.print("Number you chose: " + num + "\n" ); // prints number you chose
+        System.out.print("Number you chose: " + num + "\n"); // prints number you chose
         num *= 2; // doubles number
         System.out.println("your number doubled: " + num); // prints results
         num += 6; // adds six
         System.out.println("your number plus six: " + num); // prints results
         num /= 2; // halves
         System.out.println("your number halved: " + num); // prints results
-        num -= innum; // subracting original number
+        num -= innum; // subtracting original number
         System.out.println("your final number is: " + num); // prints final number
         System.out.println();
     }
 
-    public static void solveRiddle (int num) 
-    {
+    public static void solveRiddle(int num) {
         int innum = num;
-        System.out.print("Number you chose: " + num + "\n" ); // prints number you chose
+        System.out.print("Number you chose: " + num + "\n"); // prints number you chose
         num *= 2; // doubles number
         System.out.println("your number doubled: " + num); // prints results
         num += 6; // adds six
         System.out.println("your number plus six: " + num); // prints results
         num /= 2; // halves
         System.out.println("your number halved: " + num); // prints results
-        num -= innum; // subracting original number
+        num -= innum; // subtracting original number
         System.out.println("your final number is: " + num); // prints final number
         System.out.println();
     }
