ad8136c0068c2b56e14cc38e7d98b5b2144baf94
Tabitha <125671704+TabbyisTrans@users.noreply.github.com>
2023-11-28 18:32:52 +0000

    Completed FizzBuzz assingment

---
+++ apcsa/FizzBuzz/src/FizzBuzz.java
@@ -0,0 +1,28 @@
+public class FizzBuzz {
+    public static void main(String[] args) {
+         int i; 
+        for (i = 1; i < 101; i++)
+        {
+            if (i % 3 == 0 && i % 5 == 0) 
+            {
+                System.out.println("Fizz Buzz");
+            }
+            else 
+            {
+                if (i % 5 == 0)
+                {
+                    System.out.println("Buzz");
+                }
+                else if (i % 3 == 0)
+                {
+                    System.out.println("Fizz");
+                }
+                else
+                {
+                    System.out.println(i);
+                }
+                
+            }
+        }
+    }
+}
