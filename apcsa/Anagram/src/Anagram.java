c39bc3c61299c3648327b84ed18fe4b95fc09da1
Tabitha <125671704+TabbyisTrans@users.noreply.github.com>
2023-12-06 18:00:16 +0000

    FIXED ANAGRAM PROGRAM

--- apcsa/Anagram/src/Anagram.java
+++ apcsa/Anagram/src/Anagram.java
@@ -1,41 +1,35 @@
 import java.util.Scanner;
 
-public class Anagram
-{
-  public static void main(String[] args)
-  {
+public class Anagram {
+  public static void main(String[] args) {
     Scanner sc = new Scanner(System.in);
     System.out.println("Enter string one: ");
     String str1 = sc.nextLine();
     System.out.println("Enter string two: ");
     String str2 = sc.nextLine();
     boolean anagram = true;
-    String letter = str1.substring(0,1);
+    String letter = str1.substring(0, 1);
 
-    while (str1.length() > 0 && str2.length() >0)
-    {
-      if (str2.indexOf(letter) >= 0)
+    while (anagram && str1.length() > 1 && str2.length() > 1) {
+      System.out.println("letter: " + letter);
+      System.out.println(str2);
+      if (str2.indexOf(letter) >= 0) 
       {
         anagram = true;
-        if (str2.indexOf(letter) != str2.length() - 1)
-        {
-        str2 = str2.substring(0, str2.indexOf(letter)) + str2.substring(str2.indexOf(letter + 1));
-        }
-        if (str2.indexOf(letter) == str2.length())
-        {
-          str2.substring(0,str2.length()-1);
-        }
-
-      }
-      else 
+      } 
+      else if (str2.indexOf(letter) == -1)
       {
         anagram = false;
-      } 
-      str1.substring(1);
+      }
+      str2 = str2.substring(0, str2.indexOf(letter)) + str2.substring(str2.indexOf(letter) + 1, str2.length());
+      str1 = str1.substring(1, str1.length());
+      letter = str1.substring(0, 1);
+      System.out.println("letter: " + letter);
+
+      System.out.println("");
     }
 
-    if (str1.length() == 0 || str2.length() == 0)
-    {
+    if (str1.length() != str2.length()) {
       anagram = false;
     }
 
