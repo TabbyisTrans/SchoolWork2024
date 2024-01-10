cd39721cb407f429681502aacdd53bfa9cc67cde
Tabitha <125671704+TabbyisTrans@users.noreply.github.com>
2024-01-10 18:36:10 +0000

    Completed up to 19 of 2.5.8

---
+++ apcsa/2.5.8/src/MediaLib.java
@@ -0,0 +1,58 @@
+/**
+ * Activity 2.5.7
+ * 
+ * A MediaLib class for the MediaLibrary program
+ */
+public class MediaLib
+{
+  private Book book;
+  private Movie movie;
+  public static String owner = "PLTW";
+  private static int numEntries;
+
+  public void addBook(Book b)
+  {
+    book = b;
+    numEntries += 1;
+  }
+
+  public void addMovie(Movie b)
+  {
+    movie = b;
+    numEntries += 1;
+  }
+
+  public String toString() 
+  {
+    String info = "";
+    info += "Book: " + book + "\n"; 
+    info += "Movie: " + movie;
+    return info;
+  }
+
+  /**
+   * @param Book tester
+   */
+  void testBook(Book tester)
+  {
+    tester.setTitle("Animal Farm");
+    System.out.println(" in test: " + tester.getTitle());
+  }
+
+  public static String getOwner()
+  {
+    return owner;
+  }
+
+  public static void changeOwner(String o)
+  {
+    owner = o;
+  }
+
+  public static int getNumEntries()
+  {
+    //test
+    System.out.println("Test: Owner is " + owner);
+    return numEntries;
+  }
+}
